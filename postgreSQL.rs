bdf32b54c4e411159de29bfd2768b663538e2238
Mudada <mudada@Mudadas-MacBook-Pro.local>
2017-04-10 17:02:25 +0200

    First commit try the postgre lib

---
+++ postgreSQL.rs
@@ -0,0 +1,31 @@
+extern crate postgres;
+
+use postgres::{Connection, TlsMode};
+
+struct Spells {
+    name: String,
+    dmg: i32,
+}
+
+struct Dokemon {
+    name: String,
+    spell: Spells,
+}
+
+fn main() {
+    
+    let sadness = Spells{ name: "U re a fagt".to_string(), dmg: 12 };
+    let sad_frog = Dokemon{ name: "Pepe".to_string(), spell: sadness};
+
+    let conn = Connection::connect("postgres://postgres@localhost/trash_yard", TlsMode::None).unwrap();
+
+    conn.execute("INSERT INTO spells (name, dmg) VALUES ($1, $2)", &[&sad_frog.spell.name, &sad_frog.spell.dmg]).unwrap();
+    conn.execute("INSERT INTO dokemons (name, spell) VALUES ($1, $2)", &[&sad_frog.name, &sad_frog.spell.name]).unwrap();
+    for row in &conn.query("SELECT * FROM spells", &[]).unwrap() {
+        let spell = Spells {
+            name: row.get(0),
+            dmg: row.get(1),
+        };
+        println!("Found spell name: {}, dmg: {}.", spell.name, spell.dmg);
+    }
+}
