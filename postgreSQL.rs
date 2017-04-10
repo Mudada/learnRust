e59cf6e8aa0d5176ce7d9ab1f3a34e8e783521e9
Mudada <mudada@Mudadas-MacBook-Pro.local>
2017-04-10 22:34:28 +0200

    Hum

--- postgreSQL.rs
+++ postgreSQL.rs
@@ -12,15 +12,29 @@ struct Dokemon {
     spell: Spells,
 }
 
+fn update() -> Result<u64, postgres::error::Error> {
+    let conn = Connection::connect("postgres://postgres@localhost/trash_yard", TlsMode::None).unwrap();
+    conn.execute("UPDATE spells SET dmg = $1 WHERE dmg = $2", &[&24, &12])
+}
+
 fn main() {
-    
+
     let sadness = Spells{ name: "U re a fagt".to_string(), dmg: 12 };
     let sad_frog = Dokemon{ name: "Pepe".to_string(), spell: sadness};
 
     let conn = Connection::connect("postgres://postgres@localhost/trash_yard", TlsMode::None).unwrap();
 
-    conn.execute("INSERT INTO spells (name, dmg) VALUES ($1, $2)", &[&sad_frog.spell.name, &sad_frog.spell.dmg]).unwrap();
-    conn.execute("INSERT INTO dokemons (name, spell) VALUES ($1, $2)", &[&sad_frog.name, &sad_frog.spell.name]).unwrap();
+    //conn.execute("INSERT INTO spells (name, dmg) VALUES ($1, $2)", &[&sad_frog.spell.name, &sad_frog.spell.dmg]).unwrap();
+    //conn.execute("INSERT INTO dokemons (name, spell) VALUES ($1, $2)", &[&sad_frog.name, &sad_frog.spell.name]).unwrap();
+    for row in &conn.query("SELECT * FROM spells", &[]).unwrap() {
+        let spell = Spells {
+            name: row.get(0),
+            dmg: row.get(1),
+        };
+        println!("Found spell name: {}, dmg: {}.", spell.name, spell.dmg);
+    }
+    let updates = update().unwrap();
+    println!("{} rows were updated", updates);
     for row in &conn.query("SELECT * FROM spells", &[]).unwrap() {
         let spell = Spells {
             name: row.get(0),
